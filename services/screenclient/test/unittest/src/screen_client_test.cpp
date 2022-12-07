160b872507eebd8beb03c5124ab2f12c8cc90d4d
yangwei_814916 <yangwei271@huawei.com>
2022-12-07 10:12:19 +0800

    UT覆盖率补齐
    Signed-off-by: yangwei_814916 <yangwei271@huawei.com>

--- services/screenclient/test/unittest/src/screen_client_test.cpp
+++ services/screenclient/test/unittest/src/screen_client_test.cpp
@@ -86,6 +86,66 @@ HWTEST_F(ScreenClientTest, ShowWindow_002, TestSize.Level1)
 }
 
 /**
+ * @tc.name: ShowWindow_003
+ * @tc.desc: Verify the ShowWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, ShowWindow_003, TestSize.Level1)
+{
+    int32_t windowId = 100;
+    sptr<Surface> surface = nullptr;
+    ScreenClient::GetInstance().surfaceMap_.emplace(windowId, surface);
+    int32_t actual = ScreenClient::GetInstance().ShowWindow(windowId);
+    EXPECT_EQ(ERR_DH_SCREEN_SCREENCLIENT_SHOW_WINDOW_ERROR, actual);
+    ScreenClient::GetInstance().RemoveWindow(windowId);
+}
+
+/**
+ * @tc.name: HideWindow_001
+ * @tc.desc: Verify the HideWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, HideWindow_001, TestSize.Level1)
+{
+    int32_t windowId = 0;
+    int32_t actual = ScreenClient::GetInstance().HideWindow(windowId);
+    EXPECT_EQ(ERR_DH_SCREEN_SCREENCLIENT_SHOW_WINDOW_ERROR, actual);
+}
+
+/**
+ * @tc.name: HideWindow_002
+ * @tc.desc: Verify the HideWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, HideWindow_002, TestSize.Level1)
+{
+    std::shared_ptr<WindowProperty> windowProperty = std::make_shared<WindowProperty>();
+    int32_t windowId = ScreenClient::GetInstance().AddWindow(windowProperty);
+    int32_t actual = ScreenClient::GetInstance().HideWindow(windowId);
+    EXPECT_EQ(DH_SUCCESS, actual);
+    ScreenClient::GetInstance().RemoveWindow(windowId);
+}
+
+/**
+ * @tc.name: HideWindow_003
+ * @tc.desc: Verify the HideWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, HideWindow_003, TestSize.Level1)
+{
+    int32_t windowId = 0;
+    sptr<Surface> surface = nullptr;
+    ScreenClient::GetInstance().surfaceMap_.emplace(windowId, surface);
+    int32_t actual = ScreenClient::GetInstance().HideWindow(windowId);
+    EXPECT_EQ(ERR_DH_SCREEN_SCREENCLIENT_SHOW_WINDOW_ERROR, actual);
+    ScreenClient::GetInstance().RemoveWindow(windowId);
+}
+
+/**
  * @tc.name: MoveWindow_001
  * @tc.desc: Verify the MoveWindow function.
  * @tc.type: FUNC
@@ -118,6 +178,24 @@ HWTEST_F(ScreenClientTest, MoveWindow_002, TestSize.Level1)
 }
 
 /**
+ * @tc.name: MoveWindow_003
+ * @tc.desc: Verify the MoveWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, MoveWindow_003, TestSize.Level1)
+{
+    int32_t windowId = 0;
+    sptr<Surface> surface = nullptr;
+    ScreenClient::GetInstance().surfaceMap_.emplace(windowId, surface);
+    int32_t startX = 0;
+    int32_t startY = 0;
+    int32_t actual = ScreenClient::GetInstance().MoveWindow(windowId, startX, startY);
+    EXPECT_EQ(ERR_DH_SCREEN_SCREENCLIENT_MOVE_WINDOW_ERROR, actual);
+    ScreenClient::GetInstance().RemoveWindow(windowId);
+}
+
+/**
  * @tc.name: RemoveWindow_001
  * @tc.desc: Verify the RemoveWindow function.
  * @tc.type: FUNC
@@ -142,6 +220,22 @@ HWTEST_F(ScreenClientTest, RemoveWindow_002, TestSize.Level1)
     int32_t windowId = ScreenClient::GetInstance().AddWindow(windowProperty);
     int32_t actual = ScreenClient::GetInstance().RemoveWindow(windowId);
     EXPECT_EQ(DH_SUCCESS, actual);
+}
+
+/**
+ * @tc.name: RemoveWindow_002
+ * @tc.desc: Verify the RemoveWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, RemoveWindow_003, TestSize.Level1)
+{
+    int32_t windowId = 0;
+    sptr<Surface> surface = nullptr;
+    ScreenClient::GetInstance().surfaceMap_.emplace(windowId, surface);
+    int32_t actual = ScreenClient::GetInstance().RemoveWindow(windowId);
+    EXPECT_EQ(ERR_DH_SCREEN_SCREENCLIENT_REMOVE_WINDOW_ERROR, actual);
+    ScreenClient::GetInstance().RemoveWindow(windowId);
 }
 
 /**
@@ -171,5 +265,20 @@ HWTEST_F(ScreenClientTest, GetSurface_002, TestSize.Level1)
     EXPECT_NE(nullptr, actualSurface);
     ScreenClient::GetInstance().RemoveWindow(windowId);
 }
+
+/**
+ * @tc.name: DestroyAllWindow_001
+ * @tc.desc: Verify the DestroyAllWindow function.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(ScreenClientTest, DestroyAllWindow_001, TestSize.Level1)
+{
+    std::shared_ptr<WindowProperty> windowProperty = std::make_shared<WindowProperty>();
+    ScreenClient::GetInstance().AddWindow(windowProperty);
+    int32_t actual = ScreenClient::GetInstance().DestroyAllWindow();
+    EXPECT_EQ(DH_SUCCESS, actual);
+}
+
 } // DistributedHardware
 } // OHOS
