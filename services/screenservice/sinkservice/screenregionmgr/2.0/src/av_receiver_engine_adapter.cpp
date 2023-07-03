558ca61a5faea51d7dcd71bc06cf6c2e14676dab
yangli <yangli228@huawei.com>
2023-07-03 11:34:04 +0800

    7.3
    Signed-off-by: yangli <yangli228@huawei.com>

--- services/screenservice/sinkservice/screenregionmgr/2.0/src/av_receiver_engine_adapter.cpp
+++ services/screenservice/sinkservice/screenregionmgr/2.0/src/av_receiver_engine_adapter.cpp
@@ -137,7 +137,7 @@ int32_t AVTransReceiverAdapter::OnReceiverEvent(const AVTransEvent& event)
         case EventType::EVENT_START_SUCCESS:
             DHLOGI("av transport recevier engine start success.");
             break;
-        case EventType::EVENT_CHANNEL_CLOSED;
+        case EventType::EVENT_CHANNEL_CLOSED:
             adapterCallback_->OnEngineEvent(DScreenEventType::TRANS_CHANNEL_CLOSED, event.peerDevId);
             break;
         case EventType::EVENT_ENGINE_ERROR:
