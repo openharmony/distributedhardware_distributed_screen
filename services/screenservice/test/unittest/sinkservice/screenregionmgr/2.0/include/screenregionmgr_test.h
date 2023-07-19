3aefd71b18d0cd72b3ad5e56cf1c76d5d971e396
yangli <yangli228@huawei.com>
2023-07-19 21:18:47 +0800

    7.19
    Signed-off-by: yangli <yangli228@huawei.com>

---
+++ services/screenservice/test/unittest/sinkservice/screenregionmgr/2.0/include/screenregionmgr_test.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_SCREEN_REGION_MANAGER_TEST_V2_0_H
+#define OHOS_SCREEN_REGION_MANAGER_TEST_V2_0_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "dscreen_errcode.h"
+
+#define private public
+#include "2.0/include/screenregionmgr.h"
+#undef private
+
+namespace OHOS {
+namespace DistributedHardware {
+namespace V2_0 {
+class ScreenRegionManagerTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+} // namespace V2_0
+} // namespace DistributedHardware
+} // namespace OHOS
+#endif
---
+++ services/screenservice/test/unittest/sourceservice/dscreenmgr/2.0/src/dscreen_manager_test.cpp
@@ -0,0 +1,267 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "2.0/include/dscreen_manager_test.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace DistributedHardware {
+namespace V2_0 {
+void DScreenManagerTest::SetUpTestCase(void) {}
+
+void DScreenManagerTest::TearDownTestCase(void) {}
+
+void DScreenManagerTest::SetUp() {}
+
+void DScreenManagerTest::TearDown() {}
+
+/**
+ * @tc.name: Initialize_001
+ * @tc.desc: Verify the Init function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, Initialize_001, TestSize.Level1)
+{
+    sptr<DScreenGroupListener> dScreenGroupListener_ = new (std::nothrow) DScreenGroupListener();
+    int32_t ret = DScreenManager::GetInstance().Initialize();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: Release_001
+ * @tc.desc: Verify the Release function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, Release_001, TestSize.Level1)
+{
+    int32_t ret = DScreenManager::GetInstance().Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: HandleScreenChange_001
+ * @tc.desc: Verify the HandleScreenChange function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, HandleScreenChange_001, TestSize.Level1)
+{
+    std::shared_ptr<DScreen> changedScreen = nullptr;
+    Rosen::ScreenGroupChangeEvent event = Rosen::ScreenGroupChangeEvent::ADD_TO_GROUP;
+    DScreenManager::GetInstance().HandleScreenChange(changedScreen, event);
+
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    changedScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    DScreenManager::GetInstance().HandleScreenChange(changedScreen, event);
+
+    event = Rosen::ScreenGroupChangeEvent::REMOVE_FROM_GROUP;
+    DScreenManager::GetInstance().HandleScreenChange(changedScreen, event);
+
+    event = Rosen::ScreenGroupChangeEvent::CHANGE_GROUP;
+    DScreenManager::GetInstance().HandleScreenChange(changedScreen, event);
+
+    event = (Rosen::ScreenGroupChangeEvent)99;
+    DScreenManager::GetInstance().HandleScreenChange(changedScreen, event);
+
+    int32_t ret = DScreenManager::GetInstance().Initialize();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: StartDScreenMirror_001
+ * @tc.desc: Verify the StartDScreenMirror function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, StartDScreenMirror_001, TestSize.Level1)
+{
+    std::shared_ptr<DScreen> changedScreen = nullptr;
+    int32_t ret = DScreenManager::GetInstance().StartDScreenMirror(changedScreen);
+    EXPECT_EQ(ERR_DH_SCREEN_SA_VALUE_NOT_INIT, ret);
+
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    changedScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    ret = DScreenManager::GetInstance().StartDScreenMirror(changedScreen);
+
+    changedScreen->SetState(CONNECTING);
+    ret = DScreenManager::GetInstance().StartDScreenMirror(changedScreen);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: StopDScreenMirror_001
+ * @tc.desc: Verify the StopDScreenMirror function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, StopDScreenMirror_001, TestSize.Level1)
+{
+    std::shared_ptr<DScreen> changedScreen = nullptr;
+    int32_t ret = DScreenManager::GetInstance().StopDScreenMirror(changedScreen);
+    EXPECT_EQ(ERR_DH_SCREEN_SA_VALUE_NOT_INIT, ret);
+
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    changedScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    ret = DScreenManager::GetInstance().StopDScreenMirror(changedScreen);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    changedScreen->SetState(CONNECTING);
+    ret = DScreenManager::GetInstance().StopDScreenMirror(changedScreen);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: OnRegResult_001
+ * @tc.desc: Verify the OnRegResult function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, OnRegResult_001, TestSize.Level1)
+{
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    std::shared_ptr<DScreen> changedScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    DScreenManager::GetInstance().OnRegResult(changedScreen, "taskId000", DH_SUCCESS, "dscreen enable success.");
+
+    int32_t ret = DScreenManager::GetInstance().Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: OnUnregResult_001
+ * @tc.desc: Verify the OnUnregResult function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, OnUnregResult_001, TestSize.Level1)
+{
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    std::shared_ptr<DScreen> changedScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    DScreenManager::GetInstance().OnUnregResult(changedScreen, "taskId000", DH_SUCCESS, "dscreen disable success.");
+
+    int32_t ret = DScreenManager::GetInstance().Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: EnableDistributedScreen_001
+ * @tc.desc: Verify the EnableDistributedScreen function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, EnableDistributedScreen_001, TestSize.Level1)
+{
+    std::shared_ptr<IDScreenCallback> dScreenCallback = nullptr;
+    int32_t ret = DScreenManager::GetInstance().EnableDistributedScreen("", "", EnableParam{"", ""}, "");
+    EXPECT_EQ(ERR_DH_SCREEN_SA_ENABLE_FAILED, ret);
+
+    std::string devId = "devId000";
+    std::string dhId = "dhId000";
+    EnableParam param = {"2.0", "attrs000"};
+    std::string reqId = "reqId000";
+    ret = DScreenManager::GetInstance().EnableDistributedScreen(devId, dhId, param, reqId);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    std::shared_ptr<DScreen> dScreen = DScreenManager::GetInstance().FindDScreenById(SCREEN_ID_INVALID);
+    dScreen->SetState(DScreenState::ENABLED);
+    ret = DScreenManager::GetInstance().EnableDistributedScreen(devId, dhId, param, reqId);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    dScreen->SetState(DScreenState::DISCONNECTING);
+    ret = DScreenManager::GetInstance().EnableDistributedScreen(devId, dhId, param, reqId);
+
+    dScreen->SetState(DScreenState::ENABLING);
+    ret = DScreenManager::GetInstance().EnableDistributedScreen(devId, dhId, param, reqId);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: DisableDistributedScreen_001
+ * @tc.desc: Verify the DisableDistributedScreen function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, DisableDistributedScreen_001, TestSize.Level1)
+{
+    int32_t ret = DScreenManager::GetInstance().DisableDistributedScreen("devId111", "dhId111", "reqId111");
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    ret = DScreenManager::GetInstance().Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+/**
+ * @tc.name: FindDScreenById_001
+ * @tc.desc: Verify the FindDScreenById function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, FindDScreenById_001, TestSize.Level1)
+{
+    std::shared_ptr<DScreen> dScreen = DScreenManager::GetInstance().FindDScreenById(99);
+    EXPECT_EQ(nullptr, dScreen);
+}
+
+/**
+ * @tc.name: GetScreenDumpInfo_001
+ * @tc.desc: Verify the GetScreenDumpInfo function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, GetScreenDumpInfo_001, TestSize.Level1)
+{
+    std::string result;
+    DScreenManager::GetInstance().GetScreenDumpInfo(result);
+    EXPECT_EQ(false, result.empty());
+}
+
+/**
+ * @tc.name: GetScreenDumpInfo_002
+ * @tc.desc: Verify the GetScreenDumpInfo function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, GetScreenDumpInfo_002, TestSize.Level1)
+{
+    std::string result;
+    DScreenManager::GetInstance().GetScreenDumpInfo(result);
+    EXPECT_EQ(false, result.empty());
+}
+
+/**
+ * @tc.name: PublishMessage_001
+ * @tc.desc: Verify the PublishMessage function failed.
+ * @tc.type: FUNC
+ * @tc.require: Issue Number
+ */
+HWTEST_F(DScreenManagerTest, PublishMessage_001, TestSize.Level1)
+{
+    std::shared_ptr<IDScreenCallback> dScreenCallback = std::make_shared<DScreenCallback>();
+    std::shared_ptr<DScreen> dScreen = std::make_shared<DScreen>("devId000", "dhId000", dScreenCallback);
+    DScreenManager::GetInstance().PublishMessage(DHTopic::TOPIC_START_DSCREEN, dScreen);
+
+    DScreenManager::GetInstance().PublishMessage(DHTopic::TOPIC_STOP_DSCREEN, dScreen);
+
+    DScreenManager::GetInstance().PublishMessage(DHTopic::TOPIC_MIN, dScreen);
+
+    int32_t ret = DScreenManager::GetInstance().Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+}
+}
+}
