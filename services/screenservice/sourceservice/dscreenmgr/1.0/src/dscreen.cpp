50081f4ef7fa756db93543612096ee0266c32b2e
yangli <yangli228@huawei.com>
2023-08-15 17:07:40 +0800

    0815
    Signed-off-by: yangli <yangli228@huawei.com>

--- services/screenservice/sourceservice/dscreenmgr/1.0/src/dscreen.cpp
+++ services/screenservice/sourceservice/dscreenmgr/1.0/src/dscreen.cpp
@@ -299,9 +299,35 @@ int32_t DScreen::NegotiateCodecType(const std::string &remoteCodecInfoStr)
         }
         localCodecArray.push_back(codecInfo->GetName());
     }
-    videoParam_->SetCodecType(VIDEO_CODEC_TYPE_VIDEO_H264);
-    videoParam_->SetVideoFormat(VIDEO_DATA_FORMAT_NV12);
-    videoParam_->SetPartialRefreshFlag(true);
+    std::vector<std::string> codecTypeCandidates;
+    for (const auto &remoteCodecType : remoteCodecArray) {
+        if (std::find(localCodecArray.begin(), localCodecArray.end(),
+            remoteCodecType) != localCodecArray.end()) {
+            codecTypeCandidates.push_back(remoteCodecType);
+        }
+    }
+
+    if (std::find(codecTypeCandidates.begin(), codecTypeCandidates.end(),
+        CODEC_NAME_H264) != codecTypeCandidates.end()) {
+        videoParam_->SetCodecType(VIDEO_CODEC_TYPE_VIDEO_H264);
+        videoParam_->SetVideoFormat(VIDEO_DATA_FORMAT_NV12);
+        videoParam_->SetPartialRefreshFlag(true);
+    } else if (std::find(codecTypeCandidates.begin(), codecTypeCandidates.end(),
+        CODEC_NAME_MPEG4) != codecTypeCandidates.end()) {
+        videoParam_->SetCodecType(VIDEO_CODEC_TYPE_VIDEO_MPEG4);
+        videoParam_->SetVideoFormat(VIDEO_DATA_FORMAT_RGBA8888);
+    } else if (std::find(codecTypeCandidates.begin(), codecTypeCandidates.end(),
+        CODEC_NAME_H265) != codecTypeCandidates.end()) {
+        videoParam_->SetCodecType(VIDEO_CODEC_TYPE_VIDEO_H265);
+        videoParam_->SetVideoFormat(VIDEO_DATA_FORMAT_NV12);
+        videoParam_->SetPartialRefreshFlag(true);
+    }  else {
+        DHLOGI("codec type not support.");
+        return ERR_DH_SCREEN_SA_DSCREEN_NEGOTIATE_CODEC_FAIL;
+    }
+    // videoParam_->SetCodecType(VIDEO_CODEC_TYPE_VIDEO_H264);
+    // videoParam_->SetVideoFormat(VIDEO_DATA_FORMAT_NV12);
+    // videoParam_->SetPartialRefreshFlag(true);
     return DH_SUCCESS;
 }
 
