873c8b099a5020977a8aab62bdc889bb2e2944f8
yangli <1220629463@qq.com>
2024-01-03 17:32:51 +0800

    0103
    Signed-off-by: yangli <1220629463@qq.com>

--- common/src/dscreen_util.cpp
+++ common/src/dscreen_util.cpp
@@ -53,7 +53,10 @@ std::string GetRandomID()
     static std::uniform_int_distribution<uint64_t> dist(0ULL, 0xFFFFFFFFFFFFFFFFULL);
     uint64_t ab = dist(randomDevice);
     uint64_t cd = dist(randomDevice);
-    uint32_t a, b, c, d;
+    uint32_t a;
+    uint32_t b;
+    uint32_t c;
+    uint32_t d;
     std::stringstream stringStream;
     ab = (ab & 0xFFFFFFFFFFFF0FFFULL) | 0x0000000000004000ULL;
     cd = (cd & 0x3FFFFFFFFFFFFFFFULL) | 0x8000000000000000ULL;
