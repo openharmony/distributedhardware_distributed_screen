687d1e4f571c4bc4a3e78b46633a6013d78cd834
yangli <1220629463@qq.com>
2024-01-15 21:10:09 +0800

    0112
    Signed-off-by: yangli <1220629463@qq.com>

--- services/screenservice/sourceservice/dscreenmgr/2.0/src/dscreen.cpp
+++ services/screenservice/sourceservice/dscreenmgr/2.0/src/dscreen.cpp
@@ -334,7 +334,7 @@ void DScreen::ConsumeSurface()
     }
     int32_t retcode = syncFence_->Wait(SURFACE_SYNC_FENCE_TIMEOUT);
     if (retcode == -ETIME) {
-        DHLOGE("%s: Sync fence wait timeout, retcode is %." PRId32, retcode, LOG_TAG);
+        DHLOGE("Sync fence wait timeout, retcode is %." PRId32, retcode);
         consumerSurface_->ReleaseBuffer(surfaceBuffer, -1);
         return;
     }
