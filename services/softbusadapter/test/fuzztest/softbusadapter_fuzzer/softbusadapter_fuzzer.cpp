98d254d76caec250f89f98f0875abf092ecc223c
pwx1285814 <panzhi9@huawei.com>
2024-12-18 21:00:49 +0800

    修复fuzz告警
    Signed-off-by: pwx1285814 <panzhi9@huawei.com>
    Change-Id: Iabfe9c891b5bb4a49e8135339705a2fa99d37a5e

--- services/softbusadapter/test/fuzztest/softbusadapter_fuzzer/softbusadapter_fuzzer.cpp
+++ services/softbusadapter/test/fuzztest/softbusadapter_fuzzer/softbusadapter_fuzzer.cpp
@@ -18,6 +18,7 @@
 #include "dscreen_constants.h"
 #include "dscreen_errcode.h"
 #include "dscreen_util.h"
+#include "fuzzer/FuzzedDataProvider.h"
 #include "isoftbus_listener.h"
 #include "socket.h"
 #include "softbus_adapter.h"
@@ -27,31 +28,32 @@
 namespace OHOS {
 namespace DistributedHardware {
 namespace {
-    const std::string PEER_SESSION_NAME = "ohos.dhardware.dscreen.session8647073e02e7a78f09473aa324";
-    const std::string REMOTE_DEV_ID = "f6d4c0864707aefte7a78f09473aa122ff57fc81c00981fcf5be989e7d112324";
-    const std::string DSCREEN_PKG_NAME_TEST = "ohos.dhardware.dscreen";
+const std::string PEER_SESSION_NAME = "ohos.dhardware.dscreen.session8647073e02e7a78f09473aa324";
+const std::string REMOTE_DEV_ID = "f6d4c0864707aefte7a78f09473aa122ff57fc81c00981fcf5be989e7d112324";
+const std::string DSCREEN_PKG_NAME_TEST = "ohos.dhardware.dscreen";
 }
-void SoftbusAdapterFuzzTest(const uint8_t* data, size_t size)
+void SoftbusAdapterFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < sizeof(int32_t))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
+    uint32_t dataLen = dataProvider.ConsumeIntegral<uint32_t>();
+    unsigned int dataLen1 = dataProvider.ConsumeIntegral<unsigned int>();
 
     PeerSocketInfo peerSocketInfo = {
-        .name = const_cast<char*>(PEER_SESSION_NAME.c_str()),
-        .networkId = const_cast<char*>(REMOTE_DEV_ID.c_str()),
-        .pkgName = const_cast<char*>(DSCREEN_PKG_NAME_TEST.c_str()),
+        .name = const_cast<char *>(PEER_SESSION_NAME.c_str()),
+        .networkId = const_cast<char *>(REMOTE_DEV_ID.c_str()),
+        .pkgName = const_cast<char *>(DSCREEN_PKG_NAME_TEST.c_str()),
         .dataType = DATA_TYPE_BYTES
     };
     ShutdownReason reason = SHUTDOWN_REASON_UNKNOWN;
 
     void *adapterData = nullptr;
-    uint32_t dataLen = *(reinterpret_cast<const uint32_t*>(data));
     StreamData *stream = nullptr;
     StreamData *ext = nullptr;
     StreamFrameInfo *info = nullptr;
-    unsigned int dataLen1 = *(reinterpret_cast<const unsigned int *>(data));
 
     std::shared_ptr<SoftbusAdapter> adapter = std::make_shared<SoftbusAdapter>();
     adapter->sessListener_.OnBind(sessionId, peerSocketInfo);
@@ -64,63 +66,68 @@ void SoftbusAdapterFuzzTest(const uint8_t* data, size_t size)
     adapter->CloseSoftbusSession(sessionId);
 }
 
-void SoftbusOnBytesReceivedFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOnBytesReceivedFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
+    uint32_t dataLen = dataProvider.ConsumeIntegral<uint32_t>();
     void *adapterData = nullptr;
-    uint32_t dataLen = *(reinterpret_cast<const uint32_t*>(data));
     std::shared_ptr<SoftbusAdapter> adapter = std::make_shared<SoftbusAdapter>();
     adapter->OnBytesReceived(sessionId, adapterData, dataLen);
 }
 
-void SoftbusOnMessageReceivedFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOnMessageReceivedFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
+    unsigned int dataLen = dataProvider.ConsumeIntegral<unsigned int>();
     void *adapterData = nullptr;
-    unsigned int dataLen = *(reinterpret_cast<const unsigned int *>(data));
     std::shared_ptr<SoftbusAdapter> adapter = std::make_shared<SoftbusAdapter>();
     adapter->OnMessageReceived(sessionId, adapterData, dataLen);
 }
 
-void SoftbusOnSessionClosedFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOnSessionClosedFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
     std::shared_ptr<SoftbusAdapter> adapter = std::make_shared<SoftbusAdapter>();
     ShutdownReason reason = SHUTDOWN_REASON_UNKNOWN;
     adapter->OnSoftbusSessionClosed(sessionId, reason);
 }
 
-void SoftbusOnSessionOpenedFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOnSessionOpenedFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
     PeerSocketInfo peerSocketInfo = {
-        .name = const_cast<char*>(PEER_SESSION_NAME.c_str()),
-        .networkId = const_cast<char*>(REMOTE_DEV_ID.c_str()),
-        .pkgName = const_cast<char*>(DSCREEN_PKG_NAME_TEST.c_str()),
+        .name = const_cast<char *>(PEER_SESSION_NAME.c_str()),
+        .networkId = const_cast<char *>(REMOTE_DEV_ID.c_str()),
+        .pkgName = const_cast<char *>(DSCREEN_PKG_NAME_TEST.c_str()),
         .dataType = DATA_TYPE_BYTES
     };
     std::shared_ptr<SoftbusAdapter> adapter = std::make_shared<SoftbusAdapter>();
     adapter->OnSoftbusSessionOpened(sessionId, peerSocketInfo);
 }
 
-void SoftbusOnStreamReceivedFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOnStreamReceivedFuzzTest(const uint8_t *data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if ((data == nullptr) || (size == 0)) {
         return;
     }
-    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    FuzzedDataProvider dataProvider(data, size);
+    int32_t sessionId = dataProvider.ConsumeIntegral<int32_t>();
     StreamData *stream = nullptr;
     StreamData *ext = nullptr;
     StreamFrameInfo *info = nullptr;
@@ -128,78 +135,82 @@ void SoftbusOnStreamReceivedFuzzTest(const uint8_t* data, size_t size)
     adapter->OnStreamReceived(sessionId, stream, ext, info);
 }
 
-void SoftbusRegisterSoftbusListenerFuzzTest(const uint8_t* data, size_t size)
+void SoftbusRegisterSoftbusListenerFuzzTest(const uint8_t *data, size_t size)
 {
     if ((data == nullptr) || (size == 0)) {
         return;
     }
-    std::shared_ptr<ISoftbusListener> listener = std::make_shared<MockSoftbusListener>();
-    std::string sessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerDevId(reinterpret_cast<const char*>(data), size);
+    FuzzedDataProvider dataProvider(data, size);
+    std::string sessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerDevId(dataProvider.ConsumeRandomLengthString());
 
     std::shared_ptr<SoftbusAdapter> softbusAdapter = std::make_shared<SoftbusAdapter>();
+    std::shared_ptr<ISoftbusListener> listener = std::make_shared<MockSoftbusListener>();
     (void)softbusAdapter->RegisterSoftbusListener(listener, sessionName, peerDevId);
     (void)softbusAdapter->UnRegisterSoftbusListener(sessionName, peerDevId);
 }
 
-void SoftbusUnRegisterSoftbusListenerFuzzTest(const uint8_t* data, size_t size)
+void SoftbusUnRegisterSoftbusListenerFuzzTest(const uint8_t *data, size_t size)
 {
     if ((data == nullptr) || (size == 0)) {
         return;
     }
-    std::string sessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerDevId(reinterpret_cast<const char*>(data), size);
+    FuzzedDataProvider dataProvider(data, size);
+    std::string sessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerDevId(dataProvider.ConsumeRandomLengthString());
 
     std::shared_ptr<SoftbusAdapter> softbusAdapter = std::make_shared<SoftbusAdapter>();
     (void)softbusAdapter->UnRegisterSoftbusListener(sessionName, peerDevId);
 }
 
-void SoftbusCreateSoftbusSessionServerFuzzTest(const uint8_t* data, size_t size)
+void SoftbusCreateSoftbusSessionServerFuzzTest(const uint8_t *data, size_t size)
 {
     if ((data == nullptr) || (size == 0)) {
         return;
     }
-    std::string pkgName(reinterpret_cast<const char*>(data), size);
-    std::string sessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerDevId(reinterpret_cast<const char*>(data), size);
+    FuzzedDataProvider dataProvider(data, size);
+    std::string sessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerDevId(dataProvider.ConsumeRandomLengthString());
+    std::string pkgName(dataProvider.ConsumeRandomLengthString());
 
     std::shared_ptr<SoftbusAdapter> softbusAdapter = std::make_shared<SoftbusAdapter>();
     (void)softbusAdapter->CreateSoftbusSessionServer(pkgName, sessionName, peerDevId);
     (void)softbusAdapter->RemoveSoftbusSessionServer(pkgName, sessionName, peerDevId);
 }
 
-void SoftbusRemoveSoftbusSessionServerFuzzTest(const uint8_t* data, size_t size)
+void SoftbusRemoveSoftbusSessionServerFuzzTest(const uint8_t *data, size_t size)
 {
     if ((data == nullptr) || (size == 0)) {
         return;
     }
-    std::string pkgName(reinterpret_cast<const char*>(data), size);
-    std::string sessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerDevId(reinterpret_cast<const char*>(data), size);
+    FuzzedDataProvider dataProvider(data, size);
+    std::string sessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerDevId(dataProvider.ConsumeRandomLengthString());
+    std::string pkgName(dataProvider.ConsumeRandomLengthString());
 
     std::shared_ptr<SoftbusAdapter> softbusAdapter = std::make_shared<SoftbusAdapter>();
     (void)softbusAdapter->RemoveSoftbusSessionServer(pkgName, sessionName, peerDevId);
 }
 
-void SoftbusOpenSoftbusSessionFuzzTest(const uint8_t* data, size_t size)
+void SoftbusOpenSoftbusSessionFuzzTest(const uint8_t *data, size_t size)
 {
     if ((data == nullptr) || (size == 0)) {
         return;
     }
-    std::string mySessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerSessionName(reinterpret_cast<const char*>(data), size);
-    std::string peerDevId(reinterpret_cast<const char*>(data), size);
+    FuzzedDataProvider dataProvider(data, size);
+    std::string mySessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerSessionName(dataProvider.ConsumeRandomLengthString());
+    std::string peerDevId(dataProvider.ConsumeRandomLengthString(size));
 
     std::shared_ptr<SoftbusAdapter> softbusAdapter = std::make_shared<SoftbusAdapter>();
     int32_t socketId = softbusAdapter->OpenSoftbusSession(mySessionName, peerSessionName, peerDevId);
     (void)softbusAdapter->CloseSoftbusSession(socketId);
 }
-
-}  // namespace DistributedHardware
-}  // namespace OHOS
+} // namespace DistributedHardware
+} // namespace OHOS
 
 /* Fuzzer entry point */
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     /* Run your code on data */
     OHOS::DistributedHardware::SoftbusAdapterFuzzTest(data, size);
